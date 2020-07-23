c5ec994af629defada008eb67803558713629acf
intermezzo0475 <zhangtian0475@gmail.com>
2020-07-24 01:51:45 +0800

    new post-processing tools

--- prepost_process.py
+++ prepost_process.py
@@ -3,7 +3,7 @@
 # File              : prepost_process.py
 # Author            : tzhang
 # Date              : 25.11.2019
-# Last Modified Date: 05.12.2019
+# Last Modified Date: 24.07.2020
 # Last Modified By  : tzhang
 
 from matplotlib import pyplot as plt
@@ -66,7 +66,7 @@ class post_process:
         pltName = 'power_abondoned.png'
         plt.savefig(pltName,dpi = 100)
 
-    # plot cash flow of n years
+    # plot cash flow of a unit n years
     def plt_cashflow(n_year,cashflow,system):
         
         year = np.arange(0,n_year,1) 
@@ -81,6 +81,40 @@ class post_process:
         pltName = system+'_'+'cashflow.png'
         plt.savefig(pltName,dpi = 100)
 
+    # plot cash flow of a system with n units and cash flow of each unit in the system
+    def plt_sys_cashflow(n_year,cashflow,cashdic):
 
-    # data writer
-#    def data_writer(time,P_demand,P_to_grid,P_abandon,P_nuclear,P_windfarm)
+        year = np.arange(0,n-year,1)
+        
+        plt.figure(figsize = (14,8))
+
+        # plot overall system cash flow
+        plt.plot(year,cashflow, color = 'firebrick',linewidth = '3',marker = 'o', markersize = '5', label = 'system')
+
+        # plot cash flow of each component
+        for key in cashdic.keys():
+            plt.plot(year,cashflow[key],linewidth = '2', lable = key)
+
+        plt.xlabel('year',fontsize = '16')
+        plt.xlim(left = 0.0)
+        plt.ylabel('Cash Flow ($ in Million)', fontsize = '16')
+        plt.grid(linestyle='--',linewidth = '1')
+
+
+    # cash flow data writer
+    def data_cashflow(cashflow, cashdic):
+        
+        datafile = 'data_cashflow.txt'
+
+        with open(datafile,"w+") as f:
+            f.write('year'+'    '+'system'+'    ')
+            for key in cashdic.keys():
+                f.write(key+'   ')
+            f.write('\n')
+
+            for i in range(len(cashflow)):
+                f.write(str(i)+'    ')
+                f.write(str(cashflow[i])+'      ')
+                for key in cashdic.keys():
+                    f.write(cashdic[key][i]+'       ')
+                f.write('\n')
