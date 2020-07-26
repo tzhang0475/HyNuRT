c4bbd9dfffd54862cca1719a2787c3ed278c2e8b
intermezzo0475 <zhangtian0475@gmail.com>
2020-07-27 00:34:22 +0800

    tools used for construction plan and coupling between economic and performance

---
+++ coupling.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# File              : coupling.py
+# Author            : tzhang
+# Date              : 27.07.2020
+# Last Modified Date: 27.07.2020
+# Last Modified By  : tzhang
+
+
+"""
+
+tools used for coupling economic analysis and operation as well as construction and operation plan 
+
+"""
+
+
+"""
+
+a module to couple data between system performance and economic analysis
+
+"""
+class couple:
+    def __init__(self):
+        self.r_scale = 0.0
+
+    # a module to transfer period performance data to comparable annular performance data
+    def timescale_trans(self,time):
+        # min in a year, neglect leap year
+        min_year = 365 * 24 * 60
+
+        # get calculated performance data time scale, in min
+        time_cal = time[-1]
+
+        # ratio between calculated time and annular time
+        r_scale = min_year/time_cal
+
+        self.r_scale = r_scale
+
+    # a module calculating 
